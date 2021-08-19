5e70560839884b25bf3236645c72b24501dc79ff
ElGutiar <el12guitar@gmail.com>
2021-08-19 15:58:05 +0300

    V-02 Initial setting for order page

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
+import App from './app';
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
     document.getElementById('root')
 );
---
+++ src/routes/orderPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Header from '../../components/header'
+
+export default function OrderPage() {
+  return (
+    <main>
+      <Header />
+    </main>
+  )
+}
