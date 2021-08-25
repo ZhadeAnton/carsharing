0bd0e0c8bbe24859d740b39f4ccb49ecfb590973
ElGutiar <el12guitar@gmail.com>
2021-08-25 14:38:24 +0300

    V-04 Implement forms on the three step page

--- src/components/steps/stepThree/index.tsx
+++ src/components/steps/stepThree/index.tsx
@@ -5,12 +5,13 @@ import RadioGroup from '../../forms/radiopGroup'
 import CheckboxGroup from '../../forms/checkboxGroup'
 import DateForm from '../../forms/dateForm'
 
-const radioGroup = ['Любой', 'Красный', 'Голубой']
+const colorGroup = ['Любой', 'Красный', 'Голубой']
+const rateGroup = ['Поминутно, 7₽/мин', 'На сутки 1999 ₽/сутки']
 const checkboxGroup = ['Полный бак, 500р', 'Детское кресло, 200р', 'Правый руль, 1600р']
 
 export default function StepThree() {
   const [colorSelected, setColorSelected] = useState<string>('Любой')
-  const [rateSelected, setRateSelected] = useState<string>('Любой')
+  const [rateSelected, setRateSelected] = useState<string>('На сутки 1999 ₽/сутки')
 
   const handleColorChange = (value: string) => {
     setColorSelected(value);
@@ -22,27 +23,36 @@ export default function StepThree() {
 
   return (
     <section className='step-three step'>
-      <h6 className='step-three__title'>Цвет</h6>
+      <h6 className='step-three__title'>
+        Цвет
+      </h6>
 
       <RadioGroup
-        buttons={radioGroup}
+        buttons={colorGroup}
         selected={colorSelected}
         handleChange={handleColorChange}
       />
 
-      <h6 className='step-three__title'>Дата аренды</h6>
+      <h6 className='step-three__title'>
+        Дата аренды
+      </h6>
+
       <DateForm />
 
-      <h6 className='step-three__title'>Тариф</h6>
+      <h6 className='step-three__title'>
+        Тариф
+      </h6>
 
       <RadioGroup
-        buttons={radioGroup}
+        buttons={rateGroup}
         selected={rateSelected}
         handleChange={handleRateChange}
         vertical
       />
 
-      <h6 className='step-three__title'>Доп услуги</h6>
+      <h6 className='step-three__title'>
+        Доп услуги
+      </h6>
 
       <CheckboxGroup checkboxes={checkboxGroup} />
     </section>
